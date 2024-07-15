192246a1b7ffe5be90daf9fe9791603b9e2763fb
Josué Campos <coder.josue@gmail.com>
2024-07-15 13:17:11 -0600

    Commit 1
    Creador: Josué Campos
    Detalle: Creación de proyecto, pagina contacto e Index
    Historia de usuario: Contacto

---
+++ TiendaMuchi/src/main/java/com/Finca/FincaMuchiApplication.java
@@ -0,0 +1,13 @@
+package com.Finca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincaMuchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincaMuchiApplication.class, args);
+	}
+
+}
---
+++ TiendaMuchi/src/test/java/com/Finca/FincaMuchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Finca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincaMuchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
